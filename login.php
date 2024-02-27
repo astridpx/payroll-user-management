113cf21af92362813923a745369c44f5cfb0201b
Christian <cjvaldez151@gmail.com>
2024-02-27 11:26:03 +0800

    feat. enhance login page design

--- login.php
+++ login.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" href="./assets/css/login.css" />
+    <title>Login Page</title>
+  </head>
 
-<head>
-	<meta charset="utf-8">
-	<meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-	<title>Admin | Employee's Payroll Management System</title>
-
-
-	<?php include('./includes/header.php'); ?>
-	<?php include('./config/db_connect.php'); ?>
+	<?php include('./includes/login.php'); ?>
+  <?php include('./config/db_connect.php'); ?>
 	<?php
 	session_start();
 	if (isset($_SESSION['login_id']))
@@ -17,130 +20,67 @@
 
 	?>
 
-</head>
-<style>
-	body {
-		width: 100%;
-		height: calc(100%);
-		/*background: #007bff;*/
-	}
+  
+  <body>
+    <div class="container">
+      <div class="forms-container">
+        <div class="signin-signup">
+          <form class="sign-in-form" id="login-form">
+            <h2 class="title">Sign in</h2>
+          
+            <br>
 
-	main#main {
-		width: 100%;
-		height: calc(100%);
-		background: white;
-	}
+            
+            <div class="input-field">
+              <i class="fas fa-user" style="margin: 20px 0 0 20px;"></i>
+              <input type="text" id="username" name="username" placeholder="Enter Username ID"/>
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock" style="margin: 20px 0 0 20px;"></i>
+            	<input type="password" id="password" name="password" placeholder="Enter Password" />
+            </div><button class="btn solid" >Login</button>
+         
+          </form>
+        </div>
+      </div>
 
-	#login-right {
-		position: absolute;
-		right: 0;
-		width: 40%;
-		height: calc(100%);
-		background: white;
-		display: flex;
-		align-items: center;
-	}
+      <div class="panels-container">
+        <div class="panel left-panel">
+          <div class="content">
+            <h3> Jollibee User Management and Payroll System</h3>
+          </div>
+          <img src="img/logo.svg" class="image" alt="" style="width: 50%; margin-right: 120px;"/>
+        </div>
+      
+  </body>
 
-	#login-left {
-		position: absolute;
-		left: 0;
-		width: 60%;
-		height: calc(100%);
-		background: #59b6ec61;
-		display: flex;
-		align-items: center;
-		background: url(assets/img/payroll-cover.jpg);
-		background-repeat: no-repeat;
-		background-size: cover;
-	}
-
-	#login-right .card {
-		margin: auto;
-		z-index: 1
-	}
-
-	.logo {
-		margin: auto;
-		font-size: 8rem;
-		background: white;
-		padding: .5em 0.7em;
-		border-radius: 50% 50%;
-		color: #000000b3;
-		z-index: 10;
-	}
-
-	div#login-right::before {
-		content: "";
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: calc(100%);
-		height: calc(100%);
-		background: #9fa3a7e0;
-	}
-</style>
-
-<body>
-
-
-	<main id="main" class=" bg-dark">
-		<div id="login-left">
-
-		</div>
-
-		<div id="login-right">
-			<div class="card col-md-8">
-				<div class="card-body">
-
-					<form id="login-form">
-						<div class="form-group">
-							<label for="username" class="control-label">Username</label>
-							<input type="text" id="username" name="username" class="form-control">
-						</div>
-						<div class="form-group">
-							<label for="password" class="control-label">Password</label>
-							<input type="password" id="password" name="password" class="form-control">
-						</div>
-						<center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button></center>
-					</form>
-				</div>
-			</div>
-		</div>
-
-
-	</main>
-
-	<a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
-
-
-</body>
-<script>
-	$('#login-form').submit(function(e) {
-		e.preventDefault()
-		$('#login-form button[type="button"]').attr('disabled', true).html('Logging in...');
-		if ($(this).find('.alert-danger').length > 0)
-			$(this).find('.alert-danger').remove();
-		$.ajax({
-			url: './services/ajax.php?action=login',
-			method: 'POST',
-			data: $(this).serialize(),
-			error: err => {
-				console.log(err)
-				$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
-
-			},
-			success: function(resp) {
-				if (resp == 1) {
-					location.href = 'index.php?page=home';
-				} else if (resp == 2) {
-					location.href = 'voting.php';
-				} else {
-					$('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
-					$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
-				}
-			}
-		})
-	})
-</script>
-
+  <script src="./assets/js/logo.js"></script>
+  <script>
+    $('#login-form').submit(function(e) {
+      e.preventDefault()
+      $('#login-form button[type="button"]').attr('disabled', true).html('Logging in...');
+      if ($(this).find('.alert-danger').length > 0)
+        $(this).find('.alert-danger').remove();
+      $.ajax({
+        url: './services/ajax.php?action=login',
+        method: 'POST',
+        data: $(this).serialize(),
+        error: err => {
+          console.log(err)
+          $('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+  
+        },
+        success: function(resp) {
+          if (resp == 1) {
+            location.href = 'index.php?page=home';
+          } else if (resp == 2) {
+            location.href = 'voting.php';
+          } else {
+            $('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
+            $('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+          }
+        }
+      })
+    })
+  </script>
 </html>
