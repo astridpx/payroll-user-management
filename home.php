1e7721c1e4cb6ea0db42565211cf936998d5effe
Iskyy <lanteangelo12@gmail.com>
2024-03-04 21:29:06 +0800

    Adding graph

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 
 </style>
 
-<div class="containe-fluid">
+<div class="container-fluid  p-2">
 
 	<div class="row">
 		<div class="col-lg-12">
@@ -11,19 +11,74 @@
 		</div>
 	</div>
 
-	<div class="row mt-3 ml-3 mr-3">
+	<div class="row mt-2">
 		<div class="col-lg-12">
 			<div class="card">
 				<div class="card-body">
 					<?php echo "Welcome back " . $_SESSION['login_name'] . "!"  ?>
-
 				</div>
-
 			</div>
 		</div>
 	</div>
 
+	<div style="width: 60%; background-color: #fff; position: relative; height:60vh" class="ro mt-3 rounded-2">
+		<canvas id="chart1" class=""></canvas>
+		<!-- <canvas id="chart2" class=""></canvas> -->
+	</div>
+
 </div>
 <script>
+	const ctx1 = $('#chart1');
+	// const ctx2 = $('#chart2');
 
+	new Chart(ctx1, {
+		type: 'bar',
+		data: {
+			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 20, 3],
+				borderWidth: 1,
+			}]
+		},
+		options: {
+			aspectRatio: 16 / 9,
+			scales: {
+				x: {
+					beginAtZero: true,
+					grid: {
+						display: false, // Disable vertical grid lines for the x-axis / nakahiga
+					},
+					border: {
+						display: false
+					},
+				},
+				y: {
+					beginAtZero: true,
+					border: {
+						display: false
+					},
+				},
+			},
+			plugins: {
+				legend: {
+					display: true,
+					labels: {
+						usePointStyle: true,
+						pointStyle: "circle",
+						boxWidth: 8,
+						boxHeight: 8,
+					},
+				},
+				title: {
+					display: true,
+					text: "Total Profit",
+					font: {
+						size: 19,
+					},
+				},
+			}
+
+		},
+	});
 </script>
